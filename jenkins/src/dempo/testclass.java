db0d69b90977e1169ad589bd3d22f4dcf7c7eddc
premakr <premakr86@gmail.com>
2019-03-19 16:59:02 -0700

    commit

---
+++ jenkins/src/dempo/testclass.java
@@ -0,0 +1,14 @@
+package dempo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testclass 
+{
+	@Test
+	public void test()
+	{
+		Reporter.log("hiii",true);
+	}
+
+}
